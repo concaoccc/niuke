7b8f48c569d492f446fad9b02bc53e1919d2a449
deadfish <916548030@qq.com>
2018-06-23 10:50:23 +0800

    牛客网双体

---
+++ niuke/src/Offer/Order1.java
@@ -0,0 +1,17 @@
+package Offer;
+
+public class Order1 {
+
+	public boolean Find(int target, int [][] array) {
+
+    }
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ niuke/src/meituan/pre1.java
@@ -0,0 +1,21 @@
+package meituan;
+
+import java.io.ObjectInputStream.GetField;
+import java.util.Scanner;
+
+public class pre1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner in = new Scanner(System.in);
+        while (in.hasNextInt()) {//注意while处理多个case
+            int n = in.nextInt();
+            for (int i = 0; i < n; i++) {
+            	String tmpData = in.nextLine();
+			}
+        }
+	}
+}
